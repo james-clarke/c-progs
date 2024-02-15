6ba8c325e58c4a8695cbeded0cf4c377083039fd
James Clarke <james.clarke@tcu.edu>
2024-02-15 13:29:18 -0600

    feat: shellsort implementation

--- chapter-3/shell-sort.c
+++ chapter-3/shell-sort.c
@@ -1,6 +1,26 @@
 #include <stdio.h>
 
-int main()
-{
+// shellsort: sort v[0]...v[n-1] into ascending order
+void shellsort(int v[], int n) {
+    int gap, i, j, temp;
+
+    for (gap = n / 2; gap > 0; gap /= 2)
+        for (i = gap; i < n; i++)
+            for (j = i - gap; j >= 0 && v[j] > v[j + gap]; j -= gap) {
+                temp = v[j];
+                v[j] = v[j + gap];
+                v[j + gap] = temp;
+            }
+}
+
+int main() {
+    int arr[] = {9, 8, 7, 6, 5, 4, 3, 2, 1, 0};
+    int n = 10;
+
+    shellsort(arr, n);
+
+    for (int i = 0; i < n; i++)
+        printf("%d", arr[i]);
+
     return 0;
 }
